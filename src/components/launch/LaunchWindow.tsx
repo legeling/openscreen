4a183615893687bfaeedcdf0728eb2d2d9345bd2
Siddharth <siddharthvaddem@yahoo.com>
2026-06-02 23:33:15 -0700

    fix ci check

--- src/components/launch/LaunchWindow.tsx
+++ src/components/launch/LaunchWindow.tsx
@@ -21,12 +21,7 @@ import {
 import { RxDragHandleDots2 } from "react-icons/rx";
 import { useI18n, useScopedT } from "@/contexts/I18nContext";
 import { getAvailableLocales, getLocaleName } from "@/i18n/loader";
-import {
-	getProjectFolder,
-	loadUserPreferences,
-	parentDirectoryOf,
-	saveUserPreferences,
-} from "@/lib/userPreferences";
+import { loadUserPreferences, saveUserPreferences } from "@/lib/userPreferences";
 import { nativeBridgeClient } from "@/native";
 import { useAudioLevelMeter } from "../../hooks/useAudioLevelMeter";
 import { useCameraDevices } from "../../hooks/useCameraDevices";
